9dfd9c0d561639a0f33d140510a75e79b0b09220
VOUGY <guillaume.vouilloz@outlook.com>
2018-12-14 14:06:12 +0100

    Create RoomsController

---
+++ ValaisBookingAPI/ValaisBookingAPI/Controllers/RoomsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ValaisBookingAPI.Models;
+
+namespace ValaisBookingAPI.Controllers
+{
+    public class RoomsController : ApiController
+    {
+        private ValaisBookingEntities db = new ValaisBookingEntities();
+
+        // GET: api/Rooms
+        public IQueryable<Room> GetRooms()
+        {
+            return db.Rooms;
+        }
+
+        // GET: api/Rooms/5
+        [ResponseType(typeof(Room))]
+        public async Task<IHttpActionResult> GetRoom(int id)
+        {
+            Room room = await db.Rooms.FindAsync(id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(room);
+        }
+    }
+}
