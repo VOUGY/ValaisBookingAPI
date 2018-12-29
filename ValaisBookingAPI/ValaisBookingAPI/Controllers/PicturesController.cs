ce403053af30adb24e718080ced0e4257114db56
Ken <kjaggi94@gmail.com>
2018-12-29 19:37:31 +0100

    Add Picture and reservation controller

---
+++ ValaisBookingAPI/ValaisBookingAPI/Controllers/PicturesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ValaisBookingAPI.Models;
+
+
+namespace ValaisBookingAPI.Controllers
+{
+    public class PicturesController : ApiController
+    {
+        private ValaisBookingEntities db = new ValaisBookingEntities();
+
+        // GET: api/Hotel/{id}/Pictures
+        [Route("api/Hotels/{id}/Pictures")]
+        [HttpGet]
+        public IQueryable<Picture> GetPicturesForHotel(int id)
+        {
+            IQueryable<Picture> pictures = db.Rooms.Where(r => r.IdHotel == id).SelectMany(r=>db.Pictures);
+
+            return pictures;
+        }
+
+        // GET: api/rooms/{id}/Pictures
+        [Route("api/rooms/{id}/Pictures")]
+        [HttpGet]
+        public IQueryable<Picture> GetPicturesForRoom(int id)
+        {
+            IQueryable<Picture> pictures = db.Rooms.Where(r => r.IdRoom == id).SelectMany(r => db.Pictures);
+
+            return pictures;
+        }
+
+    }
+}
---
+++ ValaisBookingAPI/ValaisBookingAPI/Controllers/ReservationsController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ValaisBookingAPI.Models;
+
+namespace ValaisBookingAPI.Controllers
+{
+    public class ReservationsController : ApiController
+    {
+        private ValaisBookingEntities db = new ValaisBookingEntities();
+
+        // GET: api/Reservations/5
+        [ResponseType(typeof(Reservation))]
+        public IHttpActionResult GetReservation(int id)
+        {
+            Reservation reservation = db.Reservations.Find(id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(reservation);
+        }
+
+        // POST: api/Reservations
+        [ResponseType(typeof(Reservation))]
+        public async System.Threading.Tasks.Task<IHttpActionResult> PostReservationAsync(Reservation reservation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Reservations.Add(reservation);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = reservation.IdReservation }, reservation);
+        }
+
+        // GET: api/reservations/19/rooms/
+        [Route("api/reservations/{idReservation}/rooms")]
+        public IQueryable<Room> GetRoomsReservations(int idReservation)
+        {
+            return db.Reservations.Where(re => re.IdReservation == idReservation).SelectMany(re=>db.Rooms);
+        }
+
+        /*
+        // POST: api/reservations/{idReservation}/rooms/{idRoom}
+        [Route("api/reservations/{idReservation}/rooms")]
+        public IQueryable<Room> PostRoomsReservations(int idReservation, )
+        {
+            return db.Reservations.Where(re => re.IdReservation == idReservation).SelectMany(re => db.Rooms);
+        }
+        */
+
+        // DELETE: api/Reservations/5
+        [ResponseType(typeof(Reservation))]
+        public IHttpActionResult DeleteReservation(int id)
+        {
+            Reservation reservation = db.Reservations.Find(id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            db.Reservations.Remove(reservation);
+            db.SaveChanges();
+
+            return Ok(reservation);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ReservationExists(int id)
+        {
+            return db.Reservations.Count(e => e.IdReservation == id) > 0;
+        }
+    }
+}
